b227bc8f66345f3c05f17d2780a19dcc80707309
Ross William Cournoyer <rcoupvc@yahoo.com>
2018-05-25 21:35:21 +0800

    FInished

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,22 +8,30 @@ function tokenForUser(user) {
 }
 
 exports.signin = function(req, res, next) {
+    console.log(next);
     //user has had their email and password auth'd
     //we just need to give them a token
     res.send({ token: tokenForUser(req.user) })
 }
-
+// exports.signin = function(req, res, next) {
+//     User.findOne({ email: req.body.email}, function (err, existingUser) {
+//         if (err) { return next(err); }
+//         if (existingUser) {
+//             res.send({ token: tokenForUser(existingUser)});
+//         }
+//     }); 
+// }
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
     //make sure request is correctly formatted
-    if (!email || !password) return res.status(422).send({ error: 'email or password missing'});
+    if (!email || !password) return res.status(422).send('email or password missing');
 
     //check if email already exists
     User.findOne({ email: email}, function(err, existingUser) {
        if (err) return next(err);
-       if (existingUser) return res.status(422).send({ error: 'Email is in use'});
+       if (existingUser) return res.status(422).send( 'Email is in use');
     })
     //if not, make a new user from the User model
     const user =  new User({
